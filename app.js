4302e623119233936b44df228c690f896629b424
gustavocgb11@gmail.com <gustavocgb11@gmail.com>
2022-09-21 15:26:37 -0300

    Revert "novo log"
    This reverts commit 095fbea87dc0b5fd526896fc654aed7b5560ff2f.

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 console.log("Rodando o sistema")
 console.log("Aplicação está se conectando com o banco de dados")
+<<<<<<< HEAD
 console.log("Novo log")
 console.log("Novo log 2")
+=======
+>>>>>>> parent of 095fbea... novo log
