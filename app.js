11319f14de06da2d67d01c1547131462ce6101ee
gustavocgb11@gmail.com <gustavocgb11@gmail.com>
2022-09-21 15:29:33 -0300

    teste revert 2

--- app.js
+++ app.js
@@ -1,7 +1,2 @@
 console.log("Rodando o sistema")
 console.log("Aplicação está se conectando com o banco de dados")
-<<<<<<< HEAD
-console.log("Novo log")
-console.log("Novo log 2")
-=======
->>>>>>> parent of 095fbea... novo log
